a99adbbbc48e4ce4f2c481faf9fa26ebb9764917
Zhiyuwin1 <19322045@gmail.com>
2023-08-26 04:35:45 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+
+
+<template>
+
+  <div class="common-layout">
+    <el-container>
+      <el-container >
+        <daohang/>
+          <xinjianrenwu/>
+        <el-footer>Footer</el-footer>
+      </el-container>
+      <el-container>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+<script setup>
+import Xinjianrenwu from "@/components/xinjianrenwu.vue";
+import daohang from "@/components/daohang.vue";
+import Daohang from "@/components/daohang.vue";
+</script>
+
+<style scoped>
+
+</style>
